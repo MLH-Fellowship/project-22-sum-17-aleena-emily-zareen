0b3d8b330a9143401035545050b4fdf12c80fea0
Aleena <aleena@gmail.com>
2022-06-04 04:18:15 -0400

    updated homepage

--- app/__init__.py
+++ app/__init__.py
@@ -9,13 +9,20 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html', title="Portfolio Home", url=os.getenv("URL"))
+
 @app.route('/aleena-portfolio')
-def portfolio_page():
-    return render_template('aleena-portfolio.html', title="Aleena's Portfolio", url=os.getenv("URL"), author="Aleena")
-"""@app.route('/emily-portfolio')
-def index():
-    return render_template('index.html', title="Emily's Portfolio", url=os.getenv("URL"), author="Emily")
+def aleena_portfolio_page():
+    authorFirstName = "Aleena"
+    authorLastName = "Tim"
+    workExperience = [{'title': 'WiTNY Robotics Researcher', 'company': 'WiTNY Research Foundation', 'startDate': 'Sep 2018', 'endDate': 'Dec 2018'},
+                      {'title': 'Coding Instructor', 'company': 'ScholarStem', 'startDate': 'Sep 2021', 'endDate': 'May 2022'},
+                      {'title': 'WiTNY Robotics Researcher', 'company': 'WiTNY Research Foundation', 'startDate': 'Sep 2018', 'endDate': 'Dec 2018'}]
+    return render_template('aleena-portfolio.html', title="Aleena's Portfolio", url=os.getenv("URL"), authorFirstName="Aleena", authorLastName="Tim", workExperience=workExperience)
+
+@app.route('/emily-portfolio')
+def emily_portfolio_page():
+    return render_template('emily-portfolio.html', title="Emily's Portfolio", url=os.getenv("URL"), author="Emily")
+    
 @app.route('/zareen-portfolio')
-def index():
-    return render_template('index.html', title="Zaneer's Portfolio", url=os.getenv("URL"), author="Zareen")
-"""
+def portfolio_page():
+    return render_template('zareen-portfolio.html', title="Zaneer's Portfolio", url=os.getenv("URL"), author="Zareen")
