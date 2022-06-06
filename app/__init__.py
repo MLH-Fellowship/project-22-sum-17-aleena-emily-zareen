dc8e745d9fc02ac3657c27083df5e3916cfc4c38
aleena.t <38635405+AleenaTim@users.noreply.github.com>
2022-06-05 22:59:23 -0400

    Add files via upload

--- app/__init__.py
+++ app/__init__.py
@@ -1,28 +1,35 @@
-import os
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, json,  url_for, request, redirect
 from app.map_app import map_app
 import folium
+import os
+import json
 from dotenv import load_dotenv
 load_dotenv()
 app = Flask(__name__)
-
 app.register_blueprint(map_app)
+dataFile = open("C:/Users/aleen/Desktop/project-22-sum-17-aleena-emily-zareen/app/static/data.json" , encoding = "mbcs")
+
+data = json.load(dataFile)
 
 @app.route('/')
-def index():
-    return render_template('index.html', title="Portfolio Home", url=os.getenv("URL"))
+def index():    
+    allUsers = data    
+    return render_template('index.html', title="Home", allUsers=allUsers)
 
-@app.route('/aleena-portfolio')
-def aleena_portfolio_page():
-    authorFirstName = "Aleena"
-    authorLastName = "Tim"
-    workExperience = [{'title': 'WiTNY Robotics Researcher', 'company': 'WiTNY Research Foundation', 'startDate': 'Sep 2018', 'endDate': 'Dec 2018'},
-                      {'title': 'Coding Instructor', 'company': 'ScholarStem', 'startDate': 'Sep 2021', 'endDate': 'May 2022'},
-                      {'title': 'WiTNY Robotics Researcher', 'company': 'WiTNY Research Foundation', 'startDate': 'Sep 2018', 'endDate': 'Dec 2018'}]
-    return render_template('aleena-portfolio.html', title="Aleena's Portfolio", url=os.getenv("URL"), authorFirstName="Aleena", authorLastName="Tim", workExperience=workExperience)
-@app.route('/emily-portfolio')
-def emily_portfolio_page():
-    return render_template('emily-portfolio.html', title="Emily's Portfolio", url=os.getenv("URL"), author="Emily")
-@app.route('/zareen-portfolio')
-def zareen_portfolio_page():
-    return render_template('zareen-portfolio.html', title="Zaneer's Portfolio", url=os.getenv("URL"), author="Zareen")
+@app.route('/aleena-tim-portfolio')
+def aleena_portfolio():
+    allUsers = data    
+    return render_template('aleena-tim-portfolio.html', title="Aleena", allUsers=allUsers)
+
+@app.route('/emily-lai-portfolio')
+def emily_portfolio():
+    allUsers = data    
+    return render_template('emily-lai-portfolio.html', title="Emily", allUsers=allUsers)
+
+@app.route('/zareen-kabir-portfolio')
+def zareen_portfolio():
+    allUsers = data    
+    return render_template('zareen-kabir-portfolio.html', title="Zareen", allUsers=allUsers)
+ 
+if __name__ == "__main__":
+    app.run(debug=True)
